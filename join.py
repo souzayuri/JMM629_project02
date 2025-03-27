fe654b0bcfc1a073d1f0a8bca0f9e06121dc3248
Yuri Souza <desouza.s.yuri@gmail.com>
2025-03-27 11:33:34 -0400

    x

--- join.py
+++ join.py
@@ -17,7 +17,14 @@ print(coors)
 #drop duplicates
 coors = coors.drop_duplicates()
 print(coors)
+
+#%%
+# rename the names in Biome column of 'Atlantic' to 'Atlantic Forest'
+coors['Biome'] = coors['Biome'].replace('Atlantic', 'Atlantic Forest')
+
+
 # %%
 #save to csv
+
 coors.to_csv('data/df_merged_na_coord3.csv', index=False)
 # %%
